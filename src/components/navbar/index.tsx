6615b07238e28b733195a14a95f87c3456a6867b
McgrathAaron <135990095+McgrathAaron@users.noreply.github.com>
2023-12-04 10:49:23 +0000

    Update index.tsx
    Fixed minor issue with import from logo

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import React from 'react';
 import { StyledNavbar, FlexBetweenContainer, Logo, SearchIcon } from './style';
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
-import logo from '@/€assets/images/Logo.svg'
+import logo from '../../assets/images/Logo.svg'
 
 type Props = {};
 
